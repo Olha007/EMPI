f2c21ad6e3dfdcff0197ec1eae1709bf58a13c1b
olhanozdriukhina <olkaforz@gmail.com>
2022-05-04 16:42:18 +0100

    normal analiz

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
-ValueFromTable = input("Enter value from the table: ")
 changes = True
+ValueFromTable = input("Enter value from the table: ")
 n = 100
 np.random.seed()
 e = input("Enter expected value: ")
 s = input("Enter arithmetic mean deviation: ")
 x = np.random.normal(e, s, n)
-
 print(x)
 
 minValue = x.min()
-maxValue = x.max()
-average_Value = sum(x) / n
-dispersionValue = sum((i - average_Value) ** 2 for i in range(n)) / n
-sValue = n * dispersionValue / (n - 1)
-
-print(maxValue)
 print(minValue)
+
+maxValue = x.max()
+print(maxValue)
+
+average_Value = sum(x) / n
 print(average_Value)
-print(sValue)
+
+dispersionValue = sum((i - average_Value) ** 2 for i in range(n)) / n
+print(dispersionValue)
+
+sValue = n * dispersionValue / (n - 1)
+print (sValue)
 print((maxValue - average_Value) / sValue)
 print((average_Value - minValue) / sValue)
 
@@ -33,22 +35,17 @@ while changes:
     if ((maxValue - average_Value) / sValue) > ValueFromTable:
         x.max = average_Value
         maxValue = max(x)
+        print("Previous changes")
         changes = True
-        print("change was")
     else:
         changes = False
     if ((average_Value - minValue) / sValue) > ValueFromTable:
         x.min = average_Value
         minValue = min(x)
+        print("Previous changes")
         changes = True
-        print("change was")
     else:
         changes = False
-    ax.plot(x, marker="o", linestyle='')
-    plt.show()
-
 fig, ax = plt.subplots()
 ax.plot(x, marker="o", linestyle='')
 plt.show()
-
-
